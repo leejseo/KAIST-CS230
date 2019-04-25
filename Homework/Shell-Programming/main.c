766441ac2673c0b0f6a0a7550ef7666b201aabee
Jongseo Lee <while1print1@gmail.com>
2019-04-25 18:26:21 +0900

    Update and rename Homework/#2-Shell-Programming/main.c to Homework/Shell-Programming/main.c

--- Homework/Shell-Programming/main.c
+++ Homework/Shell-Programming/main.c
@@ -6,8 +6,7 @@
 
 #include "shell.h"
 
-int main()
-{   
+int main(){   
     char command[100];  // maximum input char length = 100
     while(true){
         fgets(command, sizeof(command), stdin); // get command line
