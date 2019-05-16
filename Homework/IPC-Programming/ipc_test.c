050591e726eed1d1feddabb2eda1ca844b77e121
Jongseo Lee <leejseo@kaist.ac.kr>
2019-05-16 22:40:39 +0900

    Update ipc_test.c

--- Homework/IPC-Programming/ipc_test.c
+++ Homework/IPC-Programming/ipc_test.c
@@ -10,10 +10,13 @@
 #include <sys/shm.h> 
 #include <sys/msg.h>
 
+const int MAX_SIZE = 100;
+
 typedef struct {
     long mtype;
-    char mtext[100];
+    char mtext[MAX_SIZE];
 } _msgbuf;
+
 
 /*
     Send a message via message queue
@@ -35,7 +38,7 @@ void msg_queue_send(char *msg, int prj_id) {
     mbuf.mtype = 1;
     strcpy(mbuf.mtext, msg); // copy message into msgbuf
     
-    msgsnd(msgid, &mbuf, 100, IPC_NOWAIT);
+    msgsnd(msgid, &mbuf, MAX_SIZE, IPC_NOWAIT);
 }
 
 /*
@@ -55,7 +58,7 @@ char* msg_queue_rcv(int prj_id) {
     int msgid = msgget(key, 0);
     
     _msgbuf mbuf;
-    msgrcv(msgid, &mbuf, 100, 0, IPC_NOWAIT);
+    msgrcv(msgid, &mbuf, MAX_SIZE, 0, IPC_NOWAIT);
     
     strcpy(ret, mbuf.mtext);
     return ret;
@@ -68,6 +71,15 @@ void shm_mem_send(char *msg, int prj_id) {
     /* 
     Write your code here
     */
+    int _file = open("CS230", O_RDWR|O_CREAT, 0644);
+    close(_file);
+    
+    key_t key = ftok("CS230", prj_id); // create key
+    int shmid = shmget(key, MAX_SIZE, IPC_CREAT|0644);
+    
+    char *shmaddr = (char*)shmat(shmid, NULL, 0);
+    strcpy(shmaddr, msg);
+    shmdt(shmaddr); // release
 }
 
 /*
@@ -79,6 +91,13 @@ char* shm_mem_rcv(int prj_id) {
     /* 
     Write your code here
     */
+    key_t key = ftok("CS230", prj_id); // create key
+    int shmid = shmget(key, MAX_SIZE, 0);
+    
+    char *shmaddr = (char*)shmat(shmid, NULL, 0);
+    strcpy(ret, shmaddr);
+    shmdt(shmaddr);
+    return ret;
 }
 
 /*
