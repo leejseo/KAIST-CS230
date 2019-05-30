2038ee7b890c20596790f4c7faa206d31f078e8e
Jongseo Lee <leejseo@kaist.ac.kr>
2019-05-30 18:20:02 +0900

    Update main.c

--- Homework/IPC-Programming/main.c
+++ Homework/IPC-Programming/main.c
@@ -6,7 +6,12 @@
 int main() {  
     msg_queue_send("asdf", 0);
     printf("%s\n", msg_queue_rcv(0));
-    
+    msg_queue_send("as", 0);
+    printf("%s\n", msg_queue_rcv(0));
+    printf("%d\n", msg_queue_rcv(1)==0);
     shm_mem_send("asdf", 0);
     printf("%s\n", shm_mem_rcv(0));
+    shm_mem_send("as", 0);
+    printf("%s\n", shm_mem_rcv(0));
+    printf("%d\n", shm_mem_rcv(1)==0);
 }
