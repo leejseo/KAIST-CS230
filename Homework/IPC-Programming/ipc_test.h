c77da169fa9d8fb3373e0fac825065650e6d8b99
Jongseo Lee <leejseo@kaist.ac.kr>
2019-05-30 18:20:45 +0900

    Update ipc_test.h

--- Homework/IPC-Programming/ipc_test.h
+++ Homework/IPC-Programming/ipc_test.h
@@ -1,3 +1,4 @@
+#include "ipc_test.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,23 +8,138 @@
 #include <iostream> 
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
+#include <sys/msg.h>
 
-enum IPC_TYPE{MSG_QUEUE, SHM_MEM};
-#define PATH_NAME   "CS230"
+const int MAX_SIZE = 100;
 
-// Struct for message queue 
-struct mesg_buffer { 
-    long mesg_type; 
-    char *mesg_text; 
-};
+typedef struct {
+    long mtype;
+    char mtext[MAX_SIZE];
+} _msgbuf;
 
-/* Function prototypes */
-// Message Queue
-void msg_queue_send(char *msg, int prj_id);
-char* msg_queue_rcv(int prj_id);
-// Shared Memory
-void shm_mem_send(char *msg, int prj_id);
-char* shm_mem_rcv(int prj_id);
-// General
-void ipc_send(int ipc_type, char *msg, int prj_id);
-char* ipc_rcv(int ipc_type, int prj_id);
+
+/*
+    Send a message via message queue
+    msg: Message to be sent
+    prj_id: ID to create unique ipc key
+*/
+void msg_queue_send(char *msg, int prj_id) {
+    /* 
+    Write your code here
+    */
+    // make file - copied from my code of HW3
+    int _file = open("CS230", O_RDWR|O_CREAT, 0644);
+    close(_file);
+    
+    key_t key = ftok("CS230", prj_id); // create key
+    int msgid = msgget(key, IPC_CREAT|0644); //msgid
+    
+    _msgbuf mbuf;
+    mbuf.mtype = 1;
+    strcpy(mbuf.mtext, msg); // copy message into msgbuf
+    
+    msgsnd(msgid, &mbuf, MAX_SIZE, IPC_NOWAIT); //send
+}
+
+/*
+    Receive a message via message queue
+    The program may hang due to no data, consider to handle this case
+    Be sure the IPC keys of two process are them same
+    Expected output: Message obtained from queue
+*/
+
+char ret[100];
+
+char* msg_queue_rcv(int prj_id) {
+    /* 
+    Write your code here
+    */
+    key_t key = ftok("CS230", prj_id); // create key
+    int msgid = msgget(key, 0);
+    
+    _msgbuf mbuf;
+    if (-1 == msgrcv(msgid, &mbuf, MAX_SIZE, 0, IPC_NOWAIT)){
+        return NULL;
+    }
+    strcpy(ret, mbuf.mtext);
+    return ret;
+}
+
+/*
+    Send a message via shared memory
+*/
+void shm_mem_send(char *msg, int prj_id) {
+    /* 
+    Write your code here
+    */
+    int _file = open("CS230", O_RDWR|O_CREAT, 0644);
+    close(_file);
+    
+    key_t key = ftok("CS230", prj_id); // create key
+    int shmid = shmget(key, MAX_SIZE, IPC_CREAT|0644);
+    
+    char *shmaddr = (char*)shmat(shmid, NULL, 0);
+    strcpy(shmaddr, msg);
+    shmdt(shmaddr); // release
+}
+
+/*
+    Receive a message via shared memory
+    The program may hang due to no data, consider to handle this case
+    Expected output: Message obtained from shared memory
+*/
+char* shm_mem_rcv(int prj_id) {
+    /* 
+    Write your code here
+    */
+    key_t key = ftok("CS230", prj_id); // create key
+    int shmid = shmget(key, MAX_SIZE, 0);
+    
+    void *shmaddr = shmat(shmid, NULL, 0);
+    if (shmaddr == (void*) -1) {
+        return NULL;
+    }
+    strcpy(ret, (char *)shmaddr);
+    shmdt(shmaddr); // release
+    return ret;
+}
+
+/*
+    General function for IPC sending
+    Recommended to not modify this function
+*/
+void ipc_send(int ipc_type, char *msg, int prj_id) {
+    switch (ipc_type) {
+    case MSG_QUEUE:
+        msg_queue_send(msg, prj_id);
+        break;
+    case SHM_MEM:
+        shm_mem_send(msg, prj_id);
+        break;
+    default:
+        printf("Wrong IPC type! \n");
+        break;
+    }
+}
+
+/*
+    General function for IPC receiving
+    Recommended to not modify this function
+*/
+char* ipc_rcv(int ipc_type, int prj_id) {
+    char* rcv = NULL;
+    
+    switch (ipc_type) {
+    case MSG_QUEUE:
+        rcv = msg_queue_rcv(prj_id);
+        break;
+    case SHM_MEM:
+        rcv = shm_mem_rcv(prj_id);
+        break;
+    default:
+        printf("Wrong IPC type! \n");
+        break;
+    }
+    
+    return rcv;
+}
